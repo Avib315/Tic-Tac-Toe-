0b2ad99b682708b82ed144c305aaff0676eeadca
Yehuda <yuda05246@gmail.com>
2024-05-06 11:58:24 +0300

    update some ui

--- src/components/Board/board.jsx
+++ src/components/Board/board.jsx
@@ -1,18 +1,28 @@
-import { useEffect, useState } from "react"
-import "./board.scss"
-import createBoard from "../../functions/boardCreator"
-import { Box } from "../Box/box"
+import { useEffect, useState } from "react";
+import "./board.scss";
+import createBoard from "../../functions/boardCreator";
+import { Box } from "../Box/box";
 export const Board = () => {
-    const [boxs, setBoxs] = useState(createBoard())
-    const [turn, setTurn] = useState(0)
-    const clickHendler = (b) => {
-        let input = turn % 2 == 0 ? "x" : "o"
-        setBoxs({ ...boxs, [b]: { input: input } })
-        setTurn(turn + 1)
-    }
-    return (
-        <div className="Borad">
-            {boxs.arr.map(b => <Box key={"box-" + b} disable={!true} value={boxs[b].input} clickEvent={()=>{clickHendler(b)}} />)}
-        </div>
-    )
-}
+  const [boxs, setBoxs] = useState(createBoard());
+  console.log(boxs);
+  const [turn, setTurn] = useState(0);
+  const clickHendler = (b) => {
+    let input = turn % 2 == 0 ? "x" : "o";
+    setBoxs({ ...boxs, [b]: { input: input } });
+    setTurn(turn + 1);
+  };
+  return (
+    <div className="Borad">
+      {boxs.arr.map((b) => (
+        <Box
+          key={"box-" + b}
+          disable={!true}
+          value={boxs[b].input}
+          clickEvent={() => {
+            clickHendler(b);
+          }}
+        />
+      ))}
+    </div>
+  );
+};
--- src/functions/boardCreator.js
+++ src/functions/boardCreator.js
@@ -1,8 +1,8 @@
-export default function createBoard (){
-  let board = {arr:[]}
-    for(let i = 1 ; i <= 9 ; i++){
-        board[i] = {input: null,position:i}
-        board.arr.push(i)
+export default function createBoard() {
+  let board = { arr: [] };
+  for (let i = 1; i <= 9; i++) {
+    board[i] = { input: null, position: i };
+    board.arr.push(i);
   }
-    return board
+  return board;
 }
